f024646ad9f89d165fdcad64dcab8a3da6dd2ea5
Arthur Ayvazyan <Arthur.ayvaz@gmail.com>
2021-01-08 16:56:49 +0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './scss/style.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ToDo from './homework-9/ToDo'
+import ToDo from './homework-8/ToDo'
 
 function App() {
   return (
     <div className="App">
-      {/*<div className="container">*/}
-      <ToDo />
-      {/*</div>*/}
-
+      <div className="container">
+        <ToDo />
+      </div>
     </div>
   );
 }
+
 
 export default App;
