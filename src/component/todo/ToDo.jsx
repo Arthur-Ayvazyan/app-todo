c451e49301c1db7d39605d5bfa4c60bf7e936006
Arthur Ayvazyan <Arthur.ayvaz@gmail.com>
2021-01-12 11:26:07 +0400

    homework-12 done

--- src/component/todo/ToDo.jsx
+++ src/component/todo/ToDo.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Task from './Task';
+import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
+import idGenerator from '../../helpers/idGenerator';
+
+import React, { Component } from 'react';
 import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
 import idGenerator from '../../helpers/idGenerator';
 
@@ -8,8 +13,7 @@ class ToDo extends Component {
     taskText: '',
     taskTitle: '',
     arrTasks: [],
-    arrChecked: [],
-    taskIds: []
+    selectedTasks: new Set(),
 
   }
 
@@ -38,73 +42,62 @@ class ToDo extends Component {
       taskTitle: '',
       taskText: '',
     });
-
   }
 
-  checkDone = (e, taskId) => {
-    const { arrChecked } = this.state;
-    let copyChecked = [...arrChecked];
-    if (e.target.checked) {
-      this.setState({
-        arrChecked: [...copyChecked, taskId],
-      });
-      return;
-    }
-    copyChecked = arrChecked.filter((elem) => {
-      return elem !== taskId;
-    })
+  addTaskByEnter = (e) => {
+    (e.key === "Enter") && this.addTask();
+  }
+
+  selectTasks = (taskId) => {
+    const copySelectedTasks = new Set(this.state.selectedTasks);
+    copySelectedTasks.has(taskId) ? copySelectedTasks.delete(taskId) : copySelectedTasks.add(taskId);
     this.setState({
-      arrChecked: [...copyChecked],
+      selectedTasks: copySelectedTasks,
     });
   }
 
-  removeCurrentTask = (taskId) => {
-    const { arrTasks, arrChecked } = this.state;
+  deleteCurrentTask = (taskId) => {
+    const { arrTasks } = this.state;
     const copyArrTasks = arrTasks.filter((task) => {
       return taskId !== task.id;
     });
-    const copyArrChecked = arrChecked.filter((task) => {
-      return task !== taskId;
-    });
     this.setState({
       arrTasks: copyArrTasks,
-      arrChecked: copyArrChecked
     });
   }
 
   resetAllTasks = () => {
     this.setState({
       arrTasks: [],
-      arrChecked: [],
+      selectedTasks: new Set(),
       taskTitle: '',
       taskText: '',
     });
   }
 
-  resetCheckedTasks = () => {
-    const { arrTasks, arrChecked } = this.state;
-    if (arrChecked.length === 0) return;
+  deleteSelectedTasks = () => {
+    const { arrTasks, selectedTasks } = this.state;
     const restArrTasks = arrTasks.filter((task) => {
-      return !arrChecked.includes(task.id);
+      return !selectedTasks.has(task.id);
     })
     this.setState({
       arrTasks: [...restArrTasks],
-      arrChecked: []
+      selectedTasks: new Set(),
     });
   }
 
   render() {
 
-    const { taskText, taskTitle, arrTasks } = this.state;
+    const { taskText, taskTitle, arrTasks, selectedTasks } = this.state;
     const list = arrTasks.map((task, index) => {
       return (
         <Col xs={3} id={task.id} key={task.id} className="todo__list" >
           <Card>
             <Card.Body>
-              <Form.Check type="checkbox" id={task.id} onChange={(e) => this.checkDone(e, task.id)} />
+              <Form.Check type="checkbox" id={index} onChange={() => this.selectTasks(task.id)} />
               <Card.Title>{task.title}</Card.Title>
               <Card.Text>{task.text}</Card.Text>
-              <Button variant="danger" onClick={(e) => this.removeCurrentTask(task.id)}>remove</Button>
+              <Button variant="danger" onClick={(e) => this.deleteCurrentTask(task.id)} disabled={!!selectedTasks.size}>remove</Button>
             </Card.Body>
           </Card>
         </Col >
@@ -117,12 +110,12 @@ class ToDo extends Component {
         < h1 className="todo__heading" > Create To - Do list, be more productive!</h1>
         <Row>
           <Col xs={7}>
-            <Form.Control value={taskTitle} onChange={this.setTitle} type="text" placeholder="Set task title" />
-            <Form.Control value={taskText} onChange={this.setValue} type="text" placeholder="Create new task..." />
+            <Form.Control value={taskTitle} onChange={this.setTitle} onKeyDown={this.addTaskByEnter} type="text" placeholder="Set task title" disabled={!!selectedTasks.size} />
+            <Form.Control value={taskText} onChange={this.setValue} onKeyDown={this.addTaskByEnter} type="text" placeholder="Create new task..." disabled={!!selectedTasks.size} />
           </Col>
           <Col xs={5}>
-            <Button variant={"success"} onClick={this.addTask}>Add Task</Button>
-            <Button className="todo__reset-checked" onClick={this.resetCheckedTasks} > Reset Checked Tasks</Button>
+            <Button variant={"success"} onClick={this.addTask} disabled={!!selectedTasks.size}>Add Task</Button>
+            <Button variant={"warning"} onClick={this.deleteSelectedTasks} disabled={!selectedTasks.size}>delete selected</Button>
             <Button variant={"danger"} onClick={this.resetAllTasks}>Reset All Tasks</Button>
           </Col>
 
