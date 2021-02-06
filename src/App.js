e99b0227decfdef283f9ec043d1f961680836f19
Arthur Ayvazyan <Arthur.ayvaz@gmail.com>
2021-02-06 16:52:05 +0400

    created single task page & create textCutter function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ToDo from './components/pages/todo/ToDo';
 import About from './components/pages/About/About';
 import Contact from './components/pages/Contact/Contact';
 import NotFound from './components/pages/NotFound/NotFound';
+import SingleTask from './components/pages/SingleTask/SingleTask';
 import NavMenu from './components/NavMenu/NavMenu';
 import { Container } from 'react-bootstrap';
 
@@ -11,48 +12,53 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
 
-   return (
+  return (
 
-      <div className="App">
-         <Container>
-            <BrowserRouter>
-               <NavMenu />
+    <div className="App">
+      <Container>
+        <BrowserRouter>
+          <NavMenu />
 
-               <Switch>
-                  <Route
-                     path='/'
-                     component={ToDo}
-                     exact
-                  />
-                  <Route
-                     path='/home'
-                     component={ToDo}
-                     exact
-                  />
-                  <Route
-                     path='/about'
-                     component={About}
-                     exact
-                  />
-                  <Route
-                     path='/contact'
-                     component={Contact}
-                     exact
-                  />
-                  <Route
-                     path='/not-found'
-                     component={NotFound}
-                     exact
-                  />
-                  <Redirect to='/not-found' />
+          <Switch>
+            <Route
+              path='/'
+              component={ToDo}
+              exact
+            />
+            <Route
+              path='/home'
+              component={ToDo}
+              exact
+            />
+            <Route
+              path='/task:taskId'
+              component={SingleTask}
+              exact
+            />
+            <Route
+              path='/about'
+              component={About}
+              exact
+            />
+            <Route
+              path='/contact'
+              component={Contact}
+              exact
+            />
+            <Route
+              path='/not-found'
+              component={NotFound}
+              exact
+            />
+            <Redirect to='/not-found' />
 
-               </Switch>
+          </Switch>
 
-            </BrowserRouter>
-         </Container>
+        </BrowserRouter>
+      </Container>
 
-      </div>
-   );
+    </div>
+  );
 }
 
 
