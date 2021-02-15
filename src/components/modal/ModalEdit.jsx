620f00df93723fe433bc0d6e63fd61a0879c48b7
Arthur Ayvazyan <arthur.ayvaz@gmail.com>
2021-02-15 10:57:45 +0400

    fix autofocus in modals

--- src/components/modal/ModalEdit.jsx
+++ src/components/modal/ModalEdit.jsx
@@ -7,124 +7,124 @@ import { formatDate } from '../../helpers/utils';
 
 class ModalEdit extends Component {
 
-  constructor(props) {
-    super(props);
-    const { date } = props.task;
+   constructor(props) {
+      super(props);
+      const { date } = props.task;
 
-    this.textInput = createRef();
+      this.textInput = createRef();
 
-    this.state = {
-      ...props.task,
-      date: date ? new Date(date) : new Date()
-    }
-  }
+      this.state = {
+         ...props.task,
+         date: date ? new Date(date) : new Date()
+      }
+   }
 
-  componentDidMount() {
-    this.textInput.current.focus();
-  }
+   componentDidMount() {
+      this.textInput.current.focus();
+   }
 
-  setValue = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  }
+   setValue = (e) => {
+      const { name, value } = e.target;
+      this.setState({
+         [name]: value,
+      });
+   }
 
-  setDateValue = (value) => {
-    this.setState({
-      date: value || new Date()
-    })
-  }
+   setDateValue = (value) => {
+      this.setState({
+         date: value || new Date()
+      })
+   }
 
-  editTask = () => {
-    const title = this.state.title.trim();
-    const description = this.state.description.trim();
-    const { _id, date } = this.state;
-    const { onEdit } = this.props;
+   editTask = () => {
+      const title = this.state.title.trim();
+      const description = this.state.description.trim();
+      const { _id, date } = this.state;
+      const { onEdit } = this.props;
 
-    if (!title) return;
+      if (!title) return;
 
-    const editedTask = {
-      title,
-      description,
-      _id,
-      date: formatDate(date.toISOString())
-    };
-    onEdit(editedTask);
-  }
+      const editedTask = {
+         title,
+         description,
+         _id,
+         date: formatDate(date.toISOString())
+      };
+      onEdit(editedTask);
+   }
 
-  editTaskByEnter = (e) => {
-    if (e.key === "Enter") {
-      this.editTask();
-    }
-  }
+   editTaskByEnter = (e) => {
+      if (e.key === "Enter") {
+         this.editTask();
+      }
+   }
 
-  render() {
-    const { onClose } = this.props;
-    const { title, description } = this.state;
-    this.textInput.current && this.textInput.current.focus();
-    return (
-      <Modal
-        show={true}
-        onHide={onClose}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Task editor
+   render() {
+      const { onClose } = this.props;
+      const { title, description } = this.state;
+
+      return (
+         <Modal
+            show={true}
+            onHide={onClose}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+         >
+            <Modal.Header closeButton>
+               <Modal.Title id="contained-modal-title-vcenter">
+                  Task editor
             </Modal.Title>
-        </Modal.Header>
+            </Modal.Header>
 
-        <Modal.Body>
-          <Form.Control
-            ref={this.textInput}
-            value={title}
-            type="text"
-            className={"mb-2"}
-            onChange={this.setValue}
-            onKeyPress={this.editTaskByEnter}
-            name="title"
-          />
-          <Form.Control
-            value={description}
-            as="textarea"
-            rows={3}
-            className={"mb-2"}
-            onChange={this.setValue}
-            onKeyPress={this.editTaskByEnter}
-            name="description"
+            <Modal.Body>
+               <Form.Control
+                  ref={this.textInput}
+                  value={title}
+                  type="text"
+                  className={"mb-2"}
+                  onChange={this.setValue}
+                  onKeyPress={this.editTaskByEnter}
+                  name="title"
+               />
+               <Form.Control
+                  value={description}
+                  as="textarea"
+                  rows={3}
+                  className={"mb-2"}
+                  onChange={this.setValue}
+                  onKeyPress={this.editTaskByEnter}
+                  name="description"
 
-          />
-          <DatePicker
-            minDate={new Date()}
-            selected={this.state.date}
-            onChange={this.setDateValue}
-          />
-        </Modal.Body>
+               />
+               <DatePicker
+                  minDate={new Date()}
+                  selected={this.state.date}
+                  onChange={this.setDateValue}
+               />
+            </Modal.Body>
 
-        <Modal.Footer>
-          <Button
-            variant={"success"}
-            onClick={this.editTask}
-          >
-            Save
+            <Modal.Footer>
+               <Button
+                  variant={"success"}
+                  onClick={this.editTask}
+               >
+                  Save
            </Button>
-          <Button
-            variant="warning"
-            onClick={onClose}
-          >Cancel</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+               <Button
+                  variant="warning"
+                  onClick={onClose}
+               >Cancel</Button>
+            </Modal.Footer>
+         </Modal>
+      )
+   }
 }
 
 ModalEdit.propTypes = {
-  task: PropTypes.object.isRequired,
-  onEdit: PropTypes.func.isRequired,
-  onClose: PropTypes.func.isRequired,
+   task: PropTypes.object.isRequired,
+   onEdit: PropTypes.func.isRequired,
+   onClose: PropTypes.func.isRequired,
 }
 
 export default ModalEdit;
