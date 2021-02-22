c10d41a883cd8f953afdfd186b0967ba618c5a69
Arthur Ayvazyan <arthur.ayvaz@gmail.com>
2021-02-22 22:36:15 +0400

    creted sinner for all cases

--- src/components/pages/todo/ToDo.jsx
+++ src/components/pages/todo/ToDo.jsx
@@ -10,230 +10,229 @@ import { getTasks, deleteTask, deleteTasks } from '../../../store/actions';
 
 class ToDo extends PureComponent {
 
-  state = {
-    tasks: [],
-    selectedTasks: new Set(),
-    showConfirm: false,
-    showTaskCreator: false,
-    showTaskEditor: false,
-    editableTask: null,
-  }
-
-  componentDidMount() {
-    this.props.getTasks();
-  }
-
-  componentDidUpdate(prevProps) {
-    console.log(prevProps)
-
-    if (!prevProps.addTaskSuccess && this.props.addTaskSuccess) {
-      this.setState({
-        showTaskCreator: false
-      });
-      return;
-    }
-
-    if (!prevProps.deleteTasksSuccess && this.props.deleteTasksSuccess) {
-      this.setState({
-        selectedTasks: new Set(),
-        showConfirm: false
-      });
-      return;
-    }
-
-    if (!prevProps.editTaskSuccess && this.props.editTaskSuccess) {
-      this.setState({
-        showTaskEditor: false,
-        editableTask: null,
-      });
-      return;
-    }
-  }
-
-  selectTask = (taskId) => {
-    const copySelectedTasks = new Set(this.state.selectedTasks);
-    copySelectedTasks.has(taskId) ? copySelectedTasks.delete(taskId) : copySelectedTasks.add(taskId);
-    this.setState({
-      selectedTasks: copySelectedTasks,
-    });
-  }
-
-  selectAll = () => {
-    const taskIds = this.props.tasks.map((task) => {
-      return task._id;
-    })
-    this.setState({
-      selectedTasks: new Set(taskIds),
-    });
-  }
-
-  unselectAll = () => {
-    this.setState({
+   state = {
+      tasks: [],
       selectedTasks: new Set(),
-    });
-  }
-
-  deleteSelected = () => {
-    const { selectedTasks } = this.state;
-    this.props.deleteTasks(selectedTasks)
-  }
-
-  getEditableTask = (task) => {
-    this.setState({
-      editableTask: task,
-    })
-  }
-
-  editTaskHendle = () => {
-    this.setState({
-      showTaskEditor: !this.state.showTaskEditor,
+      showConfirm: false,
+      showTaskCreator: false,
+      showTaskEditor: false,
       editableTask: null,
-    });
-  }
+   }
 
-  confirmHendle = () => {
-    this.setState({
-      showConfirm: !this.state.showConfirm,
-    })
-  }
+   componentDidMount() {
+      this.props.getTasks();
+   }
 
-  newTaskHendle = () => {
-    this.setState({
-      showTaskCreator: !this.state.showTaskCreator,
-    })
-  }
+   componentDidUpdate(prevProps) {
+      console.log(prevProps)
+
+      if (!prevProps.addTaskSuccess && this.props.addTaskSuccess) {
+         this.setState({
+            showTaskCreator: false
+         });
+         return;
+      }
+
+      if (!prevProps.deleteTasksSuccess && this.props.deleteTasksSuccess) {
+         this.setState({
+            selectedTasks: new Set(),
+            showConfirm: false
+         });
+         return;
+      }
+
+      if (!prevProps.editTaskSuccess && this.props.editTaskSuccess) {
+         this.setState({
+            showTaskEditor: false,
+            editableTask: null,
+         });
+         return;
+      }
+   }
+
+   selectTask = (taskId) => {
+      const copySelectedTasks = new Set(this.state.selectedTasks);
+      copySelectedTasks.has(taskId) ? copySelectedTasks.delete(taskId) : copySelectedTasks.add(taskId);
+      this.setState({
+         selectedTasks: copySelectedTasks,
+      });
+   }
+
+   selectAll = () => {
+      const taskIds = this.props.tasks.map((task) => {
+         return task._id;
+      })
+      this.setState({
+         selectedTasks: new Set(taskIds),
+      });
+   }
+
+   unselectAll = () => {
+      this.setState({
+         selectedTasks: new Set(),
+      });
+   }
+
+   deleteSelected = () => {
+      const { selectedTasks } = this.state;
+      this.props.deleteTasks(selectedTasks)
+   }
+
+   getEditableTask = (task) => {
+      this.setState({
+         editableTask: task,
+      })
+   }
+
+   editTaskHendle = () => {
+      this.setState({
+         showTaskEditor: !this.state.showTaskEditor,
+         editableTask: null,
+      });
+   }
+
+   confirmHendle = () => {
+      this.setState({
+         showConfirm: !this.state.showConfirm,
+      })
+   }
+
+   newTaskHendle = () => {
+      this.setState({
+         showTaskCreator: !this.state.showTaskCreator,
+      })
+   }
 
 
-  render() {
+   render() {
 
-    const { selectedTasks, editableTask, showConfirm, showTaskCreator, showTaskEditor } = this.state;
-    const { tasks } = this.props;
+      const { selectedTasks, editableTask, showConfirm, showTaskCreator, showTaskEditor } = this.state;
+      const { tasks } = this.props;
 
-    const list = tasks.map((task) => {
+      const list = tasks.map((task) => {
+         return (
+            <Col
+               xs={6}
+               md={4}
+               lg={4}
+               xl={3}
+               id={task._id}
+               key={task._id}
+            >
+               <Task
+                  task={task}
+                  onSelect={this.selectTask}
+                  selected={selectedTasks.has(task._id)}
+                  disabled={!!selectedTasks.size}
+                  onDelete={this.props.deleteTask}
+                  onShow={this.editTaskHendle}
+                  onEdit={this.getEditableTask}
+               />
+            </Col >
+         )
+      });
+
       return (
-        <Col
-          xs={6}
-          md={4}
-          lg={4}
-          xl={3}
-          id={task._id}
-          key={task._id}
-        >
-          <Task
-            task={task}
-            onSelect={this.selectTask}
-            selected={selectedTasks.has(task._id)}
-            disabled={!!selectedTasks.size}
-            onDelete={this.props.deleteTask}
-            onShow={this.editTaskHendle}
-            onEdit={this.getEditableTask}
-          />
-        </Col >
-      )
-    });
 
-    return (
+         <>
+            <Container>
+               <Row className={"mt-2 mb-2"}>
+                  <Col>
+                     <h1> Create To - Do list, be more productive!</h1>
+                  </Col>
+               </Row>
+               <Row className="justify-content-center mb-5">
+                  <Col>
+                     <Button
+                        className={"w-100"}
+                        variant={"danger"}
+                        onClick={this.confirmHendle}
+                        disabled={!selectedTasks.size}
+                     >
+                        delete selected
+             </Button>
+                  </Col >
+                  <Col>
+                     <Button
+                        className={"w-100"}
+                        variant={"warning"}
+                        onClick={this.selectAll}
+                        disabled={!tasks.length && !selectedTasks.size}
+                     >
+                        Select All
+             </Button>
+                  </Col >
+                  <Col>
+                     <Button
+                        className={"w-100"}
+                        variant={"warning"}
+                        onClick={this.unselectAll}
+                        disabled={!selectedTasks.size}
+                     >
+                        Unselect All
+             </Button>
+                  </Col >
+               </Row>
 
-      <>
-        <Container>
-          <Row className={"mt-2 mb-2"}>
-            <Col>
-              <h1> Create To - Do list, be more productive!</h1>
-            </Col>
-          </Row>
-          <Row className="justify-content-center mb-5">
-            <Col>
-              <Button
-                className={"w-100"}
-                variant={"danger"}
-                onClick={this.confirmHendle}
-                disabled={!selectedTasks.size}
-              >
-                delete selected
-             </Button>
-            </Col >
-            <Col>
-              <Button
-                className={"w-100"}
-                variant={"warning"}
-                onClick={this.selectAll}
-                disabled={!tasks.length && !selectedTasks.size}
-              >
-                Select All
-             </Button>
-            </Col >
-            <Col>
-              <Button
-                className={"w-100"}
-                variant={"warning"}
-                onClick={this.unselectAll}
-                disabled={!selectedTasks.size}
-              >
-                Unselect All
-             </Button>
-            </Col >
-          </Row>
-
-          <Row className={"justify-content-end"}>
-            <Col xs="auto mb-3">
-              <Button
-                variant="primary"
-                onClick={this.newTaskHendle}
-                disabled={selectedTasks.size}
-              >
-                Create Task
+               <Row className={"justify-content-end"}>
+                  <Col xs="auto mb-3">
+                     <Button
+                        variant="primary"
+                        onClick={this.newTaskHendle}
+                        disabled={selectedTasks.size}
+                     >
+                        Create Task
                </Button>
-            </Col>
+                  </Col>
 
-          </Row>
-          <Row>
-            {list}
-          </Row>
+               </Row>
+               <Row>
+                  {list}
+               </Row>
 
-          {
-            showConfirm && <Confirm
-              onClose={this.confirmHendle}
-              onDeleteTasks={this.deleteSelected}
-              deletableTasksSize={selectedTasks.size}
-            />
-          }
+               {
+                  showConfirm && <Confirm
+                     onClose={this.confirmHendle}
+                     onDeleteTasks={this.deleteSelected}
+                     deletableTasksSize={selectedTasks.size}
+                  />
+               }
 
-          {
-            showTaskCreator &&
-            <NewTask
-              onClose={this.newTaskHendle}
-            />
-          }
-          {
-            showTaskEditor &&
-            <ModalEdit
-              task={editableTask}
-              //onEdit={this.editTask}
-              onClose={this.editTaskHendle}
-            />
-          }
+               {
+                  showTaskCreator &&
+                  <NewTask
+                     onClose={this.newTaskHendle}
+                  />
+               }
+               {
+                  showTaskEditor &&
+                  <ModalEdit
+                     task={editableTask}
+                     //onEdit={this.editTask}
+                     onClose={this.editTaskHendle}
+                  />
+               }
 
-        </Container >
-      </>
-    );
-  }
+            </Container >
+         </>
+      );
+   }
 }
 
 
 const mapSateToProps = (state) => {
-  return {
-    tasks: state.tasks,
-    addTaskSuccess: state.addTaskSuccess,
-    deleteTasksSuccess: state.deleteTasksSuccess,
-    editTaskSuccess: state.editTaskSuccess,
-
-  };
+   return {
+      tasks: state.tasks,
+      addTaskSuccess: state.addTaskSuccess,
+      deleteTasksSuccess: state.deleteTasksSuccess,
+      editTaskSuccess: state.editTaskSuccess,
+   };
 }
 
 const mapDispatchToProps = {
-  getTasks,
-  deleteTask,
-  deleteTasks,
+   getTasks,
+   deleteTask,
+   deleteTasks,
 }
 
 export default connect(mapSateToProps, mapDispatchToProps)(ToDo);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,100 +1,82 @@
 
 const defaultState = {
-  tasks: [],
-  addTaskSuccess: false,
-  deleteTasksSuccess: false,
-  editTaskSuccess: false,
+   tasks: [],
+   addTaskSuccess: false,
+   deleteTasksSuccess: false,
+   editTaskSuccess: false,
+   showSpinner: false,
 }
 
 export default function reducer(state = defaultState, action) {
 
-  switch (action.type) {
-    case 'INCREMENT_COUNT': {
-      return {
-        ...state,
-        count: state.count + 1,
+   switch (action.type) {
+
+
+      case 'PENDING': {
+         return {
+            ...state,
+            addTaskSuccess: false,
+            deleteTasksSuccess: false,
+            editTaskSuccess: false,
+            showSpinner: true
+         }
       }
-    }
-
-    case 'DECREMENT_COUNT': {
-      return {
-        ...state,
-        count: state.count - 1,
+      case 'GET_TASKS': {
+         return {
+            ...state,
+            tasks: action.tasks,
+            showSpinner: false,
+         }
       }
-    }
 
-    case 'GET_TASKS': {
-      return {
-        ...state,
-        tasks: action.tasks,
+      case 'ADD_TASK': {
+         return {
+            ...state,
+            tasks: [...state.tasks, action.task],
+            addTaskSuccess: true,
+            showSpinner: false,
+         }
       }
-    }
 
-    case 'ADD_TASK': {
-      return {
-        ...state,
-        tasks: [...state.tasks, action.task],
-        addTaskSuccess: true
+      case 'DELETE_TASK': {
+         const newTasks = state.tasks.filter((task) => {
+            return action.taskId !== task._id;
+         });
+         return {
+            ...state,
+            tasks: newTasks,
+            showSpinner: false,
+         }
       }
-    }
 
-    case 'ADDING_TASK': {
-      return {
-        ...state,
-        addTaskSuccess: false
+      case 'DELETE_TASKS': {
+         const restTasks = state.tasks.filter((task) => {
+            return !action.taskIds.has(task._id);
+         })
+         return {
+            ...state,
+            tasks: restTasks,
+            deleteTasksSuccess: true,
+            showSpinner: false,
+         }
       }
-    }
 
-    case 'DELETE_TASK': {
-      const newTasks = state.tasks.filter((task) => {
-        return action.taskId !== task._id;
-      });
-      return {
-        ...state,
-        tasks: newTasks,
+      case 'EDIT_TASK': {
+         const newTasks = [...state.tasks];
+         const index = newTasks.findIndex((elem) => {
+            return elem._id === action.editedTask._id
+         });
+
+         newTasks[index] = action.editedTask;
+         return {
+            ...state,
+            tasks: newTasks,
+            editTaskSuccess: true,
+            showSpinner: false,
+         }
       }
-    }
 
-    case 'DELETE_TASKS': {
-      const restTasks = state.tasks.filter((task) => {
-        return !action.taskIds.has(task._id);
-      })
-      return {
-        ...state,
-        tasks: restTasks,
-        deleteTasksSuccess: true
-      }
-    }
-
-    case 'DELETEING_TASKS': {
-      return {
-        ...state,
-        deleteTasksSuccess: false
-      }
-    }
-
-    case 'EDIT_TASK': {
-      const newTasks = [...state.tasks];
-      const index = newTasks.findIndex((elem) => {
-        return elem._id === action.editedTask._id
-      });
-
-      newTasks[index] = action.editedTask;
-      return {
-        ...state,
-        tasks: newTasks,
-        editTaskSuccess: true
-      }
-    }
-
-    case 'EDITING_TASK': {
-      return {
-        ...state,
-        editTaskSuccess: false
-      }
-    }
-
-    default:
-      return state;
-  }
+      default:
+         return state;
+   }
 }
